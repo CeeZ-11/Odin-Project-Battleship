5acd0a97d6004fea35b4f19146041d9144c69909
Seamor Estrabon <seamorestrabon@gmail.com>
2024-12-31 07:59:42 +0800

    :sparkle: feat: Add GameController module and UI layout in html
    :sparkle: feat: GC module will generate  player and computer
    
    :bug: fix js script link

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import { Gameboard } from "./gameboard";
 import { Player } from "./player";
 import { Ship } from "./ship";
+
+const GameController = () => {
+  const player = new Player("Player");
+  const computer = new Player("Computer");
+};
